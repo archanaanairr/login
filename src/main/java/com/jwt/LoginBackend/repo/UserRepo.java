c124c096359a5981ba13a073838852e23501a736
unknown <basilgeorge2002@gmail.com>
2024-08-30 00:05:55 +0530

    initial commit

---
+++ src/main/java/com/jwt/LoginBackend/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package com.jwt.LoginBackend.repo;
+
+import com.jwt.LoginBackend.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+
+}
---
+++ src/test/java/com/jwt/LoginBackend/LoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.LoginBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
